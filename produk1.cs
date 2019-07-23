6f6a03c0bcfcab114a5437b8d66e1cf5ad28daba
rmaysi <isyarahman00@gmail.com>
2019-07-23 16:58:28 +0700

    UAS1

---
+++ produk1.cs
@@ -0,0 +1,23 @@
+﻿​using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectProduk
+
+{
+public class Produk
+
+{
+
+public string kode_Produk { get; set; }
+public string nama_Produk { get; set; }
+public int harga_Jual { get; set; }
+public int harga_Beli { get; set; }
+
+// TODO: lengkapi property class produk
+
+ }
+
+}
